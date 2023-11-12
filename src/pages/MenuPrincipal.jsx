7e582c8cd4e266ab68a90e66ec16c10f7bcac014
Pedro <dark1473@gmail.com>
2023-11-11 20:33:01 -0600

    Muestra mensaje en home

--- src/pages/MenuPrincipal.jsx
+++ src/pages/MenuPrincipal.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Auth from './Auth';
 import stylees from '../css/MenuPrincipal.css';
 import ComponenteLayout from './ComponenteLayout';
 import bootstrap from 'bootstrap';
 export default function MenuPrincipal() {
+  const [isAuthenticated, setIsAuthenticated] = useState(null); // Usar null para indicar "cargando"
+ 
+
+
+  useEffect(() => {
+    Auth()
+      .then((isAuthenticated) => {
+        setIsAuthenticated(isAuthenticated);
+      })
+      .catch((error) => {
+        console.log('Error en la solicitud:', error);
+        setIsAuthenticated(false);
+      });
+  }, []);
+
+
   return (
 
     <ComponenteLayout Titulo="Home">
@@ -10,7 +27,9 @@ export default function MenuPrincipal() {
       <div class="Principalprincipal">
 
         <div class="PrincipalcontenedorP">
-
+          {isAuthenticated ? (<h1>Bienvenido</h1>):
+          (<><h1>Bienvenido al Sistema de *inserte nombre*  del CASEI</h1><h2>Por favor inicie sesion para continuar</h2></>)}
+          
 
         </div>
 
